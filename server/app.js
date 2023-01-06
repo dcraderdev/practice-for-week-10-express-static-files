e8ca500f1d825007a9f0984e9e88702f4f44c9c2
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-01-05 16:32:58 -0800

    complete
    complete

--- server/app.js
+++ server/app.js
@@ -2,5 +2,24 @@ const express = require('express');
 const app = express();
 // Your code here
 
-const port = 5000;
+const port = 5005;
 app.listen(port, () => console.log('Server is listening on port', port));
+
+app.use('/', express.static('assets'));
+app.use('/', express.static('assets/scripts'));
+// app.use('/stylesheets', express.static('assets/css'));
+app.use("/stylesheets", express.static(`${__dirname}/assets/css`));
+
+
+
+// home page
+app.get('/assets/css/application.css', (req, res,) => {
+  console.log('Route handler');
+  // res.status(200).send('Hello for now')
+  res.sendFile(__dirname + '/assets/css/application.css');
+  // res.send('Welcome')
+});
+
+
+// app.use(express.static(`${__dirname}/assets/scripts`));
+// app.use("/scripts", express.static(`assets/scripts`));
